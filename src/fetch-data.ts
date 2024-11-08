6070323e6d598124f1208f90212471d47dac6349
Victor Carnaval <victorlopescarnaval@gmail.com>
2024-11-08 12:00:37 -0300

    fix(fetch-data): fix status code validation to correctly handle 2xx range responses

--- src/fetch-data.ts
+++ src/fetch-data.ts
@@ -1,6 +1,6 @@
 export const fetchData = (url: string, config: Record<string, any>) => {
     return fetch(url, config).then(res => {
-        if (res.status === 200) {
+        if (res.status >= 200 && res.status < 300) {
             const contentType = res.headers.get('content-type');
             if (contentType && contentType.indexOf('application/json') !== -1) {
                 return res.json();
