b04dff0e69bfc11fdeb41377e218a2c5a4bd0cbc
Victor Carnaval <victorlopescarnaval@gmail.com>
2024-11-08 11:52:21 -0300

    fix(uuid): correct import of uuid v4 to resolve module export issue

--- src/clients/soap-client.ts
+++ src/clients/soap-client.ts
@@ -1,41 +1,41 @@
-import {XRoadClient} from "./client";
-import {IXRoadSoapRequest} from "../types/IXRoadSoapRequest";
-import {fetchData} from "../fetch-data";
-import {IXRoadService} from "../types/IXRoadService";
-import uuidv4 from "uuid/v4";
-import parser from "fast-xml-parser";
+import { XRoadClient } from './client';
+import { IXRoadSoapRequest } from '../types/IXRoadSoapRequest';
+import { fetchData } from '../fetch-data';
+import { IXRoadService } from '../types/IXRoadService';
+import { v4 as uuidv4 } from 'uuid';
+import parser from 'fast-xml-parser';
 
 const parserOptions = {
     parseNodeValue: false,
-    ignoreAttributes : true,
-    trimValues: false
+    ignoreAttributes: true,
+    trimValues: false,
 };
 
 export class XRoadSoapClient extends XRoadClient<IXRoadSoapRequest> {
-
     public request = (req: IXRoadSoapRequest) => {
         return this.soapRequest(req);
     };
 
     public soapRequest = (req: IXRoadSoapRequest): Promise<any> => {
-
         return fetchData(this.securityServerUrl, {
             method: 'POST',
             headers: {
-                "Content-Type": 'text/xml',
-                ...(req.headers || {})
+                'Content-Type': 'text/xml',
+                ...(req.headers || {}),
             },
             agentOptions: this.agentOptions,
-            body: this.createXRoadSoapEnvelopeBody(req.service, req.body)
+            body: this.createXRoadSoapEnvelopeBody(req.service, req.body),
         }).then(body => {
             return parser.parse(body as string, parserOptions);
         });
     };
 
-    createXRoadSoapEnvelopeBody = (xroadService: IXRoadService, bodyContent: string) => {
+    createXRoadSoapEnvelopeBody = (
+        xroadService: IXRoadService,
+        bodyContent: string
+    ) => {
         // Get data from SEFAZ
-        return (
-            `<SOAP-ENV:Envelope
+        return `<SOAP-ENV:Envelope
             xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
             xmlns:xrd="http://x-road.eu/xsd/xroad.xsd"
             xmlns:id="http://x-road.eu/xsd/identifiers">
@@ -47,10 +47,14 @@ export class XRoadSoapClient extends XRoadClient<IXRoadSoapRequest> {
                     <id:subsystemCode>${this.subsystemCode}</id:subsystemCode>
                 </xrd:client>
                 <xrd:service id:objectType="SERVICE">
-                    <id:xRoadInstance>${xroadService.xRoadInstance}</id:xRoadInstance>
+                    <id:xRoadInstance>${
+                        xroadService.xRoadInstance
+                    }</id:xRoadInstance>
                     <id:memberClass>${xroadService.memberClass}</id:memberClass>
                     <id:memberCode>${xroadService.memberCode}</id:memberCode>
-                    <id:subsystemCode>${xroadService.subsystemCode}</id:subsystemCode>
+                    <id:subsystemCode>${
+                        xroadService.subsystemCode
+                    }</id:subsystemCode>
                     <id:serviceCode>${xroadService.serviceCode}</id:serviceCode>
                 </xrd:service>
                 <xrd:protocolVersion>4.0</xrd:protocolVersion>
@@ -59,7 +63,6 @@ export class XRoadSoapClient extends XRoadClient<IXRoadSoapRequest> {
             <SOAP-ENV:Body>
                 ${bodyContent}
             </SOAP-ENV:Body>
-        </SOAP-ENV:Envelope>`
-        );
+        </SOAP-ENV:Envelope>`;
     };
 }
